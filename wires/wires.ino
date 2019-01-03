0f1b91b4bf9c1112d8fd9d7c903570fd67164715
Guilherme Freire <guilhermebfreire@gmail.com>
2019-01-02 22:59:40 -0200

    + First prototype with BLUE wire

--- wires/wires.ino
+++ wires/wires.ino
@@ -1,23 +1,30 @@
 const int sensor_pin = 0;
 const int led_pin = 2;
 
+const int allowed_std = 5;
+
+// Wire color-voltage pairs
+const int BLUE_VOLT = 336;
+
 int connected = 1;
 int voltage = -1;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(led_pin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Read analog input
   voltage = analogRead(sensor_pin);
-  if(voltage > 0){
-    digitalWrite(led_pin, HIGH);
-  }
-  else{
+
+  // LED off if wire is still connected
+  if(BLUE_VOLT + allowed_std > voltage && BLUE_VOLT - allowed_std < voltage){
     digitalWrite(led_pin, LOW);
+  }
+  // LED on if wire is "cut"
+  else{
+    digitalWrite(led_pin, HIGH);
   }
   Serial.println(voltage);
 }
